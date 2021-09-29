634931dec457f59cec44e6af8911bda938e6968f
Shachar Shemesh <shachar@shemesh.biz>
2021-09-29 06:07:06 +0300

    Verify data bus as well

--- wd65c02.srcs/sim_1/new/sim_computer.v
+++ wd65c02.srcs/sim_1/new/sim_computer.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company:
-// Engineer:
+// Company: Some Assembly Required Youtube channel https://www.youtube.com/channel/UCp5Z7utSI2IHQUsnkPH41bw
+// Engineer: Shachar Shemesh
 //
 // Create Date: 09/18/2021 08:06:23 PM
-// Design Name:
+// Design Name: WD65C02S core almost compatible design 
 // Module Name: sim_computer
-// Project Name:
-// Target Devices:
-// Tool Versions:
+// Project Name: CompuSAR
+// Target Devices: Xilinx Spartan-7
+// Tool Versions: Vivado 2021.1
 // Description:
 //
 // Dependencies:
@@ -63,23 +63,24 @@ wd65c02 cpu(
 
 reg [7:0]memory[65535:0];
 
-localparam BitsInExpectedResult = 48;
-localparam OpcodeBitsHigh = 47;
-localparam OpcodeBitsLow = 47;
 /*
     Structure:
     Bits        Meaning
     47:44       line type:
-    0 - opcode
-    1 - expected bus
-    2 - set pin state
-    3 - wait
+    0 - opcode - immediate
+    1 - opcode - read
+    2 - opcode - write
+    3 - expected bus
+    4 - set pin state
+    5 - wait
 
     Bit layout for opcode type
 
-    43:40       Number of cycles in command
-    39:32       Opcode
-    31:16       First address bus access
+    55:52       Event type
+    51:48       Number of cycles in command
+    47:40       Opcode
+    39:24       First address bus access
+    23:16       Data bus on last access (only read and write operations)
     15:0        Last address bus access
 
     Bit layout for set pin type
@@ -88,6 +89,11 @@ localparam OpcodeBitsLow = 47;
     Bit layout for wait type
     39:0        Number of picoseconds to wait
  */
+localparam BitsInExpectedResult = 56;
+localparam OperationBitsHigh = 55;
+localparam OperationBitsLow = 52;
+localparam OpcodeBitsHigh = 47;
+localparam OpcodeBitsLow = 40;
 reg [BitsInExpectedResult-1:0]expected_results[7000:0];
 
 initial begin
@@ -122,9 +128,14 @@ initial begin
     results_index = 0;
     while( expected_results[results_index] != EndMarker ) begin
         current_command = expected_results[results_index];
-        case( current_command[OpcodeBitsHigh:OpcodeBitsLow] )
+        case( current_command[OperationBitsHigh:OperationBitsLow] )
         4'h0: expect_opcode();
-        default: $display("Expected result %d is of unknown type %x at time %t", results_index, current_command[OpcodeBitsHigh:OpcodeBitsLow], $time);
+        4'h1: expect_opcode();
+        4'h2: expect_opcode();
+        default: begin
+            $display("Expected result %d is of unknown type %x at time %t", results_index, current_command[OperationBitsHigh:OperationBitsLow], $time);
+            $finish;
+        end
         endcase
 
         results_index = results_index+1;
@@ -139,18 +150,30 @@ begin
     opcode_cycle_count = 0;
     @(posedge clock)
     assert(sync, 0); // "Opcode does not start on fetch cycle"
-    assert( address_bus==current_command[31:16], 1 ); // "Command read from wrong address"
+    assert( address_bus==current_command[39:24], 1 ); // "Command read from wrong address"
     @(negedge clock)
-    assert( data_bus_in == current_command[39:32], 2 ); // "Wrong command read"
+    assert( data_bus_in == current_command[47:40], 2 ); // "Wrong command read"
     opcode_cycle_count = 1;
-    while( opcode_cycle_count < current_command[43:40] ) begin
+    while( opcode_cycle_count < current_command[51:48] ) begin
         @(posedge clock)
         assert(!sync, 3 ); // "Opcode shorter than expected"
         
         @(negedge clock)
         opcode_cycle_count = opcode_cycle_count+1;
     end
+    
     assert( current_command[15:0] == address_bus, 4 ); // "Last address bus different than expected"
+    case( current_command[OperationBitsHigh:OperationBitsLow] )
+    4'h0: ; // nothing
+    4'h1: begin
+        assert( data_bus_rW, 5 ); // "Data bus in write when it should be in read mode"
+        assert( current_command[23:16] == data_bus_in, 6 ); // "Last access data incorrect"
+    end
+    4'h2: begin
+        assert( ~data_bus_rW, 7 ); // "Data bus in read when it should be in write mode"
+        assert( current_command[23:16] == data_bus_out, 8 ); // "Last access data incorrect"
+    end
+    endcase
 end
 endtask
 
