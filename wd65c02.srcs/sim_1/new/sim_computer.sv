db3d018aa0b75e3a2a4e7c527fd4babff3d033d7
Shachar Shemesh <shachar@shemesh.biz>
2021-11-01 07:39:34 +0200

    Phrasing corrections

--- wd65c02.srcs/sim_1/new/sim_computer.sv
+++ wd65c02.srcs/sim_1/new/sim_computer.sv
@@ -17,24 +17,6 @@
 // Revision 0.01 - File Created
 // Additional Comments:
 //
-// License:
-//   Copyright (C) 2021.
-//   Copyright owners listed in AUTHORS file.
-//
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; either version 2 of the License, or
-//   (at your option) any later version.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
-//
 //////////////////////////////////////////////////////////////////////////////////
 
 
@@ -164,7 +146,7 @@ initial begin
         results_index = results_index+1;
     end
 
-    $display("Verified simulation run ended at %t after verifying %d events", $time, results_index);
+    $display("Verified simulation run successful at %t after verifying %d events", $time, results_index);
     $finish();
 end
 
